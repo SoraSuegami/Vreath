981040afc3c009c8e8c15cbbee3abcc305be9730
Sora Suegami <suegamisora@gmail.com>
2018-02-03 13:06:46 +0900

    finish developing crypto functions

--- script/crypto_set.js
+++ script/crypto_set.js
@@ -1,23 +1,12 @@
 var crypto = require("crypto");
 var NodeRSA = require("node-rsa");
 var fs = require("fs");
-/*
-var key = new NodeRSA({b: 512});
-var Private = key.exportKey('pkcs8-private-pem');
-console.log(Private);
-var Public = key.exportKey('pkcs8-public-pem');
-console.log(Public);
-var password = "Pass!";
-var cipher = crypto.createCipher('aes-256-cbc', password);
-var crypted = cipher.update(Private, 'utf-8', 'hex');
-crypted += cipher.final('hex');
-console.log(crypted);
-var decipher = crypto.createDecipher('aes-256-cbc', password);
-var dec = decipher.update(crypted, 'hex', 'utf-8');
-dec += decipher.final('utf-8');
-console.log(dec);
-*/
-var HashFromPass = (password) =>{
+
+var BofPri = /^-----BEGIN PRIVATE KEY-----/;
+var EofPri = /-----END PRIVATE KEY-----$/;
+var BofPub = /^-----BEGIN PRIVATE KEY-----/;
+var EofPub = /-----END PUBLIC KEY-----$/;
+function HashFromPass(password){
   var sha256 = crypto.createHash('sha256');
   sha256.update(password);
   var pre_hash = sha256.digest('hex');
@@ -26,7 +15,7 @@ var HashFromPass = (password) =>{
   var hash = sha512.digest('hex');
   return hash;
 }
-var GenerateKeys = (password) =>{
+function GenerateKeys(password){
   var key = new NodeRSA({b: 512});
   var Private = key.exportKey('pkcs8-private-pem');
   var Public = key.exportKey('pkcs8-public-pem');
@@ -44,9 +33,9 @@ var GenerateKeys = (password) =>{
     private:edit_private,
     public:edit_public
   }
-};
+}
 
-var PullMyPrivate = (password) =>{
+function PullMyPrivate(password){
   var hash = HashFromPass(password);
   var filename = "./keys/private/"+hash+".txt";
   var private_file = fs.readFileSync(filename,'utf-8');
@@ -58,15 +47,15 @@ var PullMyPrivate = (password) =>{
   return dec.replace(/^-----BEGIN PRIVATE KEY-----/,"").replace(/-----END PRIVATE KEY-----$/,"");
 }
 
-var PullMyPublic = (password) =>{
+function PullMyPublic(password){
   var hash = HashFromPass(password);
   var filename = "./keys/public/"+hash+".txt";
   var public_file = fs.readFileSync(filename,'utf-8');
   console.log(public_file.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,""));
   return public_file.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,"");
-};
+}
 
-var PublicFromPrivate = (Private) =>{
+function PublicFromPrivate(Private){
   if(Private.match(/^-----BEGIN PUBLIC KEY-----/)&&Private.match(/-----END PUBLIC KEY-----$/)){
     var key = new NodeRSA(Private);
   }
@@ -77,13 +66,65 @@ var PublicFromPrivate = (Private) =>{
   var edit_public =  Public.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,"");
   console.log(edit_public);
   return edit_public
-};
+}
 
-/*console.log(key);
-var text = 'Hello RSA!';
-var encrypted = key.encrypt(text, 'base64');
-console.log('encrypted: ', encrypted);
-var decrypted = key.decrypt(encrypted, 'utf8');
-console.log('decrypted: ', decrypted);
-var sign = key.sign(text);
-console.log(key.verify(text, sign));*/
+function EncryptData(data,Public){
+  if(data==null)return false;
+  else if(Public.match(BofPub)==null&&Public.match(EofPub)==null) {
+    Public = '-----BEGIN PUBLIC KEY-----\n'+Public+'-----END PUBLIC KEY-----';
+    var key = new NodeRSA(Public);
+  }
+  else{
+    var key = new NodeRSA(Public);
+  }
+  var encrypted = key.encrypt(data, 'base64');
+  console.log('encrypted: ', encrypted);
+  return encrypted;
+}
+
+function DecryptData(data,Private){
+  if(data==null)return false;
+  else if (Private.match(BofPri)==null&&Private.match(EofPri)==null) {
+    Private = '-----BEGIN PRIVATE KEY-----\n'+Private+'-----END PRIVATE KEY-----'
+    var key = new NodeRSA(Private);
+  }
+  else{
+    console.log(Private);
+    var key = new NodeRSA(Private);
+  }
+  var decrypted = key.decrypted(data,'utf-8');
+  console.log('decrypted: ', decrypted);
+  return decrypted;
+}
+
+function SignData(data,password){
+  if(data==null)return false;
+  Private = PullMyPrivate(password);
+  Private = '-----BEGIN PRIVATE KEY-----\n'+Private+'-----END PRIVATE KEY-----'
+  var key = new NodeRSA(Private);
+  var sign = key.sign(data);
+  return sign;
+}
+function verifyData(data,sign,Public){
+  if(data==null||sign==null)return false;
+  else if(Public.match(BofPub)==null&&Public.match(EofPub)==null) {
+    Public = '-----BEGIN PUBLIC KEY-----\n'+Public+'-----END PUBLIC KEY-----';
+    var key = new NodeRSA(Public);
+  }
+  else{
+    var key = new NodeRSA(Public);
+  }
+  return key.verify(data, sign);
+}
+
+module.exports ={
+  HashFromPass:HashFromPass,
+  GenerateKeys:GenerateKeys,
+  PullMyPrivate:PullMyPrivate,
+  PullMyPublic:PullMyPublic,
+  PublicFromPrivate:PublicFromPrivate,
+  EncryptData:EncryptData,
+  DecryptData:DecryptData,
+  SignData:SignData,
+  verifyData:verifyData
+};

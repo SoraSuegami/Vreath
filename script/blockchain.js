2de286f8314a4b622ba9cf5998b449a1b78df3c3
Sora Suegami <suegamisora@gmail.com>
2018-02-18 00:05:53 +0900

    began developing blockchain

---
+++ script/blockchain.js
@@ -0,0 +1,419 @@
+var Trie = require('merkle-patricia-tree');
+var Proof = require('merkle-patricia-tree/proof');
+var fs = require('fs');
+var CryptoSet = require('./crypto_set.js');
+var crypto = require("crypto");
+var levelup = require('levelup');
+var leveldown = require('leveldown');
+var rlp = require('rlp');
+var db = levelup(leveldown('./db'));
+var currency_name = 'nix';
+var txlimit = 10;
+
+var beneficiaryPub = CryptoSet.PullMyPublic(password);
+var beneficiary = CryptoSet.AddressFromPublic(beneficiaryPub);
+try{
+  var AccountState = JSON.parse(fs.readFileSync('./BlockChainData/PhoenixAccountState.json', 'utf8'));
+}catch(err){
+  var AccountState = {};
+  fs.writeFile('./BlockChainData/PhoenixAccountState.json', JSON.stringify(AccountState),function(err){
+    if (err) {
+        throw err;
+    }
+  });
+}
+console.log(AccountState);
+
+try{
+  var Pool = JSON.parse(fs.readFileSync('./BlockChainData/PhoenixTransactionPool.json', 'utf8'));
+}catch(err){
+  var Pool = [];
+  fs.writeFile('./BlockChainData/PhoenixTransactionPool.json', JSON.stringify(Pool),function(err){
+    if (err) {
+        throw err;
+    }
+  });
+}
+console.log(Pool);
+
+try{
+  var BlockChain = JSON.parse(fs.readFileSync('./BlockChainData/PhoenixBlockChain.json', 'utf8'));
+}catch(err){
+  var BlockChain = [];
+  fs.writeFile('./BlockChainData/PhoenixBlockChain.json', JSON.stringify(BlockChain),function(err){
+    if (err) {
+        throw err;
+    }
+  });
+}
+console.log(BlockChain);
+
+
+function toHash(str){
+  var sha256 = crypto.createHash('sha256');
+  sha256.update(str);
+  var pre_hash = sha256.digest('hex');
+  var sha512 = crypto.createHash('sha512');
+  sha512.update(pre_hash);
+  var hash = sha512.digest('hex');
+  return hash;
+}
+
+class AccountState{
+  constructor(address){
+    this.address = address;
+    if(this.nonce==null){
+      this.nonce=0;
+    }
+    if(this.balance==null){
+      this.balance = {[currency_name]:100};
+    }
+  }
+  SendMoney(nonce,value){
+    if(this.nonce != nonce-1){
+      throw new Error("Your nonce is wrong");
+      return false;
+    }
+    else{
+      this.nonce ++;
+    }
+    for(var kind in value){
+      if(this.balance==null || this.balance[kind]==null){
+        throw new Error("Sender don't have enough "+kind);
+        return false;
+      }
+      else if(this.balance[kind] < value[kind]){
+        throw new Error("Sender don't have enough "+kind);
+        return false;
+      }
+      else{
+        this.balance[kind] -= value[kind];
+      }
+    }
+    AccountState[this.address] = {
+      address:this.address,
+      nonce:this.nonce,
+      balance:this.balance
+    };
+    fs.writeFile('./BlockChainData/PhoenixAccountState.json', JSON.stringify(AccountState),function(err){
+      if (err) {
+          throw err;
+      }
+    });
+    ChangeWorld(this);
+  }
+  ReceiveMoney(value){
+    for(var kind in value){
+      if(this.balance==null || this.balance[kind]==null){
+        this.balance[kind] = value[kind];
+      }
+      else{
+        this.balance[kind] += value[kind];
+      }
+    }
+    AccountState[this.address] = {
+      address:this.address,
+      nonce:this.nonce,
+      balance:this.balance
+    };
+    fs.writeFile('./BlockChainData/PhoenixAccountState.json', JSON.stringify(AccountState),function(err){
+      if (err) {
+          throw err;
+      }
+    });
+    ChangeWorld(this);
+  }
+  Json(){
+    return{
+      address:this.address,
+      nonce:this.nonce,
+      balance:this.balance
+    };
+  }
+};
+
+var World = new Trie(db);
+function ChangeWorld(acstate){
+  var address = Buffer.from(acstate.address,'utf-8');
+  var state = rlp.encode(JSON.stringify(acstate.Json()));
+  World.put(address,state,function(){
+    console.log(World.root.toString('hex'));
+  });
+}
+
+
+class Tx{
+  constructor(from,from_key,to,to_key,value,gas,timestamp,nonce,hash,signature,timelock={bigin:null,end:null}) {
+    this.from = from;
+    this.from_key = from_key;
+    this.to = to;
+    this.to_key = to_key;
+    this.value = value;
+    this.gas = gas;
+    this.timestamp = timestamp;
+    this.nonce = nonce;
+    this.hash = hash.toString();
+    this.signature = signature;
+    this.timelock = timelock;
+  }
+  Json(){
+    return{
+      from:this.from,
+      from_key:this.from_key,
+      to:this.to,
+      to_key:this.to_key,
+      value:this.value,
+      gas:this.gas,
+      timestamp:this.timestamp,
+      nonce:this.nonce,
+      hash:this.hash,
+      signature:this.signature,
+      timelock:this.timelock
+    }
+  }
+  ChangeAcState(){
+    var sender = SetMyState(this.from);
+    try{
+      sender.SendMoney(this.nonce,this.value);
+      var receiver = SetMyState(this.to);
+      receiver.ReceiveMoney(this.value);
+    }catch(err){
+      console.log(err);
+    }
+    console.log(AccountState);
+  }
+  inValidTx(){
+    if(this.from!=CryptoSet.AddressFromPublic(this.from_key)){
+      console.error("invalid sender's publicKey");
+      return false;
+    }
+    else if (this.to!=CryptoSet.AddressFromPublic(this.to_key)) {
+      console.error("invalid receiver's publicKey");
+      return false;
+    }
+    else if(this.value<0){
+      console.error("invalid value");
+      return false;
+    }
+    else if(this.nonce!=SetMyState(this.address).nonce+1){
+      console.error("invalid nonce");
+      return false;
+    }
+    else if (calculateHashForTx(this)!=this.hash){
+      console.error("invalid hash");
+      return false;
+    }
+    else if (CryptoSet.verifyData(this.hash,this.signature,this.from_key)==false){
+      console.error("invalid signature");
+      return false;
+    }
+    else if(this.timelock!=null&&this.timelock.bigin!=null&&this.timelock.end!=null&&this.timelock.bigin>=this.timelock.end){
+      console.error("invalid timelock");
+      return false;
+    }
+    return true;
+  }
+}
+
+function SetMyState(address){
+  var my_state;
+  if(AccountState[address] == null){
+    my_state = new AccountState(address);
+    my_state.nonce = 0;
+    my_state.balance = {[currency_name]:0};
+    AccountState[address] = my_state.Json();
+  }
+  else{
+    my_state = new AccountState(address);
+    my_state.nonce = AccountState[address].nonce;
+    //console.log(my_state.nonce);
+    my_state.balance = AccountState[address].balance;
+  }
+  return my_state;
+}
+
+function calculateHashForTx(tx){
+  if(tx.from==null||tx.from_key==null||tx.to==null||tx.to_key==null||tx.value==null||tx.gas==null||tx.timestamp==null||tx.nonce==null){
+    console.error("invalid tx data");
+    return false;
+  }
+  if(tx.timelock==null){
+    tx.timelock="";
+  }
+  var hash = toHash(tx.from+tx.from_key+tx.to+tx.to_key+JSON.stringify(tx.value)+JSON.stringify(tx.gas)+tx.timestamp+tx.nonce+JSON.stringify(tx.timelock));
+  return hash;
+}
+
+function CreateTx(password,from,from_key,to,to_key,value,gas,timelock={bigin:null,end:null}){
+  var date = new Date();
+  var timestamp = date.getTime();
+  var nonce = SetMyState(from).nonce+1;
+  var hash = toHash(from+from_key+to+to_key+JSON.stringify(value)+JSON.stringify(gas)+timestamp+nonce+JSON.stringify(timelock));
+  var signature = CryptoSet.SignData(hash,password);
+  var NewTx = new Tx(from,from_key,to,to_key,value,gas,timestamp,nonce,hash,signature,timelock);
+  return NewTx;
+}
+
+
+
+class Block{
+  constructor(index,parentHash,Hash,timestamp,txnum,beneficiary,beneficiaryPub,stake,dags,gassum,signature,transactionsRoot,stateRoot,transactions){
+    this.index = index;
+    this.parentHash = parentHash;
+    this.Hash = Hash;
+    this.timestamp = timestamp;
+    this.txnum = txnum;
+    this.beneficiary = beneficiary;
+    this.beneficiaryPub = beneficiaryPub;
+    this.stake = stake;
+    this.dags = dags;
+    this.gassum = gassum;
+    this.signature = signature;
+    this.transactionsRoot = transactionsRoot;
+    this.stateRoot = stateRoot;
+    this.transactions = transactions;
+  }
+  Json(){
+    return{
+      header:{
+        index:this.index,
+        parentHash:this.parentHash,
+        Hash:this.Hash;
+        timestamp:this.timestamp,
+        txnum:this.txnum,
+        beneficiary:this.beneficiary,
+        beneficiaryPub:this.beneficiaryPub,
+        stake:this.stake,
+        dags:this.dags,
+        gassum:this.gassum,
+        signature:this.signature,
+        transactionsRoot:this.transactionsRoot,
+        stateRoot:this.stateRoot
+      },
+      transactions:this.transactions
+    }
+  }
+  inValidBlock(){
+    var date = new Date();
+    var acstate = new AccoutState(beneficiary);
+    if(this.index!=BlockChain.length){
+      console.error("invalid index");
+      return false;
+    }
+    else if(this.parentHash!=BlockChain[this.index-1].Hash){
+      console.error("invalid parentHash");
+      return false;
+    }
+    else if(this.timestamp>date.getTime()){
+      console.error("invalid timestamp");
+      return false;
+    }
+    else if (this.txnum!=this.transactions.length) {
+      console.error("invalid txnum");
+      return false;
+    }
+    else if (acstate==null){
+      console.error("invalid beneficiary");
+      return false;
+    }
+    else if (acstate.balance==null||acstate.balance[currency_name]==null||this.stake>acstate.balance[currency_name]) {
+      console.error("invalid stake");
+      return false;
+    }
+    else if (this.transactionsRoot!=GetTreeroot(this.transactions)){
+      console.error("invalid transactionsRoot");
+      return false;
+    }
+    else if (this.stateRoot!=World.root.toString('hex')){
+      console.error("invalid stateRoot");
+      return false;
+    }
+    else if
+    (this.Hash!=toHash(this.index+this.parentHash+this.timestamp+this.beneficiary+this.beneficiaryPub+this.stake+this.dags+this.txnum+JSON.stringify(this.gassum)+this.transactionsRoot+this.stateRoot)) {
+      onsole.error("invalid hash");
+      return false;
+    }
+    else if (CryptoSet.verifyData(this.Hash,this.signature,this.beneficiaryPub)==false) {
+      console.error("invalid signature");
+      return false;
+    }
+    return true;
+  }
+  Add(){
+    if(this.inValidBlock()==false) return false;
+    BlockChain.push(this.Json());
+    fs.writeFile('./BlockChainData/PhoenixBlockChain.json', JSON.stringify(BlockChain),function(err){
+      if (err) {
+          throw err;
+      }
+    });
+    for(tx of transactions){
+      tx.ChangeAcState();
+    }
+    return this.Json();
+  }
+}
+
+function AddtoPool(tx,password){
+  Pool.push(tx);
+  if(tx.inValidTx()==false) return false;
+  else if(Pool.length==txlimit){
+    var new_block = MakeBlock(Pool,beneficiary,stake,dags,password);
+    Pool.shift();
+  }
+  else if(Pool.length>txlimit){
+    var yet_tx = Pool.slice(0,txlimit);
+    var new_block = MakeBlock(yet_tx,beneficiary,stake,dags,password);
+    Pool.splice(0,txlimit);
+  }
+}
+
+function GetTreeroot(txs) {
+  if(txs==null)return false;
+  var pre;
+  for(var tx of txs){
+    pre.push(tx.hash);
+  }
+  var next;
+  while (pre.length>1) {
+    next = [];
+    for (var i = 0; i < pre.length; i++){
+      i = Number(i);
+      if(i%2==1)continue;
+      var left = pre[i];
+      var right;
+      if(pre[i+1]==null) right = toHash("");
+      else right = pre[i+1];
+      next.push(toHash(left+right));
+    }
+    pre = next;
+  }
+  return pre[0];
+}
+
+function MakeBlock(transactions,beneficiary,beneficiaryPub,stake,dags,password) {
+  var index = BlockChain.length;
+  var parentHash = BlockChain[index-1].Hash;
+  var date = new Date();
+  var timestamp = date.getTime();
+  var txnum = transactions.length;
+  var gassum = {};
+  for(var tx of transactions){
+    var gas = tx.gas;
+    for(var kind in gas){
+      if(gassum[kind]==null){
+        gassum[kind]=gas[kind];
+      }
+      else{
+        gassum[kind] += gas[kind];
+      }
+    }
+  }
+  var transactionsRoot = GetTreeroot(transactions);
+  var stateRoot = World.root.toString('hex');
+  var Hash = toHash(index+parentHash+timestamp+beneficiary+beneficiaryPub+stake+dags+txnum+JSON.stringify(gassum)+transactionsRoot+stateRoot);
+  var signature = CryptoSet.SignData(Hash,password);
+  var make_block =
+  new Block(index,parentHash,Hash,timestamp,txnum,beneficiary,beneficiaryPub,stake,dags,gassum,signature,transactionsRoot,stateRoot,transactions);
+  return make_block;
+}
--- script/crypto_set.js
+++ script/crypto_set.js
@@ -1,11 +1,15 @@
 var crypto = require("crypto");
-var NodeRSA = require("node-rsa");
 var fs = require("fs");
-
-var BofPri = /^-----BEGIN PRIVATE KEY-----/;
-var EofPri = /-----END PRIVATE KEY-----$/;
-var BofPub = /^-----BEGIN PRIVATE KEY-----/;
-var EofPub = /-----END PUBLIC KEY-----$/;
+var secp256k1 = require('secp256k1');
+/*
+function test(){
+  console.log("Hello");
+  return 1;
+}
+function FuncStr(func){
+  console.log(func.toString());
+}
+FuncStr(test);*/
 function HashFromPass(password){
   var sha256 = crypto.createHash('sha256');
   sha256.update(password);
@@ -15,6 +19,259 @@ function HashFromPass(password){
   var hash = sha512.digest('hex');
   return hash;
 }
+
+function GenerateKeys(password){
+  let Private
+  do {
+    Private = crypto.randomBytes(32)
+  } while (!secp256k1.privateKeyVerify(Private));
+  var Public = secp256k1.publicKeyCreate(Private);
+  var cipher = crypto.createCipher('aes-256-cbc', password);
+  var crypted = cipher.update(Private.toString('hex'), 'hex', 'hex');
+  crypted += cipher.final('hex');
+  var hash = HashFromPass(password);
+  var private_filename = "./keys/private/"+hash+".txt";
+  var public_filename = "./keys/public/"+hash+".txt";
+  fs.writeFileSync(private_filename,crypted,'hex');
+  fs.writeFileSync(public_filename,Public.toString('hex'),'hex');
+  return{
+    private:Private,
+    public:Public
+  }
+}
+//console.log(GenerateKeys('Sora'));
+
+function PullMyPrivate(password){
+  var hash = HashFromPass(password);
+  var filename = "./keys/private/"+hash+".txt";
+  var private_file = fs.readFileSync(filename,'hex');
+  var decipher = crypto.createDecipher('aes-256-cbc', password);
+  var dec = decipher.update(private_file, 'hex', 'hex');
+  dec += decipher.final('hex');
+  return Buffer.from(dec,'hex');
+}
+
+function PullMyPublic(password){
+  var hash = HashFromPass(password);
+  var filename = "./keys/public/"+hash+".txt";
+  var public_file = fs.readFileSync(filename,'hex');
+  return Buffer.from(public_file,'hex');
+}
+
+function PublicFromPrivate(Private){
+  var Public = secp256k1.publicKeyCreate(Private);
+  return Public;
+}
+//console.log(PullMyPrivate("Sora"));
+//console.log(PullMyPublic("Sora"));
+//console.log(PublicFromPrivate(PullMyPrivate("Sora")));
+
+function EncryptData(data,mypass,Public){
+  if(data==null)return false;
+  var Private = PullMyPrivate(mypass);
+  var secret = secp256k1.ecdh(Public,Private);
+  var cipher = crypto.createCipher('aes-256-cbc', secret);
+  var crypted = cipher.update(data, 'utf-8', 'hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+var ecdh = crypto.createECDH('secp256k1');
+//GenerateKeys("test");
+//EncryptData("HelloWorld","Sora",PullMyPublic("test"))
+
+function DecryptData(data,mypass,Public){
+  if(data==null)return false;
+  var Private = PullMyPrivate(mypass);
+  var secret = secp256k1.ecdh(Public,Private);
+  var decipher = crypto.createDecipher('aes-256-cbc', secret);
+  var dec = decipher.update(data, 'hex', 'utf-8');
+  dec += decipher.final('utf-8');
+  return dec;
+}
+
+//console.log(DecryptData(EncryptData("HelloWorld","Sora",PullMyPublic("test")),"test",PullMyPublic("Sora")));
+
+function SignData(data,password){
+  if(data==null)return false;
+  var Private = PullMyPrivate(password);
+  data = crypto.createHash("sha256").update(data).digest();
+  var sign = secp256k1.sign(data,Private);
+  return sign.signature;
+}
+
+function verifyData(data,sign,Public){
+  if(data==null||sign==null)return false;
+  data = crypto.createHash("sha256").update(data).digest();
+  var verify = secp256k1.verify(data,sign,Public);
+  return verify
+}
+
+function AddressFromPublic(Public) {
+  var hashed = HashFromPass(Public).substr(0,60);
+  hashed = HashFromPass(hashed).substr(0,30);
+  var address = "PH"+hashed;
+  return address;
+}
+
+function AppAddress(name){
+  var add;
+  if(Buffer.byteLength(name,'utf-8')>30){
+    name.substr(0,30);
+    add=28;
+  }
+  else if(Buffer.byteLength(name,'utf-8')>=0){
+    add=58-Buffer.byteLength(name,'utf-8');
+  }
+  else{
+    return false;
+  }
+  var hashed = HashFromPass(name).substr(0,add);
+  var address = 'PS' + name + hashed;
+  return address;
+}
+
+module.exports ={
+  HashFromPass:HashFromPass,
+  GenerateKeys:GenerateKeys,
+  PullMyPrivate:PullMyPrivate,
+  PullMyPublic:PullMyPublic,
+  PublicFromPrivate:PublicFromPrivate,
+  EncryptData:EncryptData,
+  DecryptData:DecryptData,
+  SignData:SignData,
+  verifyData:verifyData,
+  AddressFromPublic:AddressFromPublic,
+  AppAddress:AppAddress
+};
+
+/*
+function GenerateKeys(password){
+  var Private = crypto.randomBytes(32);
+  var Public = eccrypto.getPublic(Private);
+  var cipher = crypto.createCipher('aes-256-cbc', password);
+  var crypted = cipher.update(Private.toString('hex'), 'hex', 'hex');
+  crypted += cipher.final('hex');
+  var hash = HashFromPass(password);
+  var private_filename = "./keys/private/"+hash+".txt";
+  var public_filename = "./keys/public/"+hash+".txt";
+  fs.writeFileSync(private_filename,crypted,'hex');
+  fs.writeFileSync(public_filename,Public.toString('hex'),'hex');
+  //var edit_private = Private.replace(/^-----BEGIN PRIVATE KEY-----/,"").replace(/-----END PRIVATE KEY-----$/,"");
+  //var edit_public =  Public.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,"");
+  return{
+    private:Private,
+    public:Public
+  }
+}
+console.log(GenerateKeys("Sora"));
+
+
+function PullMyPrivate(password){
+  var hash = HashFromPass(password);
+  var filename = "./keys/private/"+hash+".txt";
+  var private_file = fs.readFileSync(filename,'hex');
+  decipher = crypto.createDecipher('aes-256-cbc', password);
+  dec = decipher.update(private_file, 'hex', 'hex');
+  dec += decipher.final('hex');
+  return Buffer.from(dec,'hex');
+}
+
+function PullMyPublic(password){
+  var hash = HashFromPass(password);
+  var filename = "./keys/public/"+hash+".txt";
+  var public_file = fs.readFileSync(filename,'hex');
+  return Buffer.from(public_file,'hex');
+}
+
+
+function PublicFromPrivate(Private){
+  var Public = eccrypto.getPublic(Private);
+  return Public;
+}
+
+console.log(PullMyPrivate("Sora"));
+console.log(PublicFromPrivate(PullMyPrivate("Sora")));
+
+function EncryptData(data,Public){
+  if(data==null)return false;
+  var encrypt_data;
+  eccrypto.encrypt(Public, Buffer(data)).then(function(encrypted){
+    encrypt_data = encrypted;
+  }).catch(function(err){
+    console.log(err);
+  });
+  console.log(encrypt_data);
+  return encrypted;
+}
+EncryptData("Hello",PullMyPublic("Sora"));
+
+function DecryptData(data,Private){
+  if(data==null)return false;
+  var decrypt_data;
+  eccrypto.decrypt(Private,data).then(function(decrypted){
+    decrypt_data = decrypted;
+  });
+  return decrypt_data.toString();
+}
+
+function SignData(data,password){
+  if(data==null)return false;
+  var Private = PullMyPrivate(password);
+  data = crypto.createHash("sha256").update(data).digest();
+  eccrypto.sign(Private, data).then(function(sign){
+    var sign_data = sign;
+  });
+  return sign_data;
+}
+
+function verifyData(data,sign,Public){
+  if(data==null||sign==null)return false;
+  eccrypto.verify(Public, data, sign).then(function(){
+    return true;
+  }).catch(function(){
+    return false;
+  });
+}
+
+function AddressFromPublic(Public) {
+  var hashed = HashFromPass(Public).substr(0,60);
+  hashed = HashFromPass(hashed).substr(0,30);
+  var address = "PH"+hashed;
+  return address;
+}
+
+function AppAddress(name){
+  var add;
+  if(Buffer.byteLength(name,'utf-8')>30){
+    name.substr(0,30);
+    add=28;
+  }
+  else if(Buffer.byteLength(name,'utf-8')>=0){
+    add=58-Buffer.byteLength(name,'utf-8');
+  }
+  else{
+    return false;
+  }
+  var hashed = HashFromPass(name).substr(0,add);
+  var address = 'PS' + name + hashed;
+  return address;
+}
+
+module.exports ={
+  HashFromPass:HashFromPass,
+  GenerateKeys:GenerateKeys,
+  PullMyPrivate:PullMyPrivate,
+  PullMyPublic:PullMyPublic,
+  PublicFromPrivate:PublicFromPrivate,
+  EncryptData:EncryptData,
+  DecryptData:DecryptData,
+  SignData:SignData,
+  verifyData:verifyData,
+  AddressFromPublic:AddressFromPublic,
+  AppAddress:AppAddress
+};
+*/
+/*
 function GenerateKeys(password){
   var key = new NodeRSA({b: 512});
   var Private = key.exportKey('pkcs8-private-pem');
@@ -39,11 +296,9 @@ function PullMyPrivate(password){
   var hash = HashFromPass(password);
   var filename = "./keys/private/"+hash+".txt";
   var private_file = fs.readFileSync(filename,'utf-8');
-  console.log(private_file);
   decipher = crypto.createDecipher('aes-256-cbc', password);
   dec = decipher.update(private_file, 'hex', 'utf-8');
   dec += decipher.final('utf-8');
-  console.log(dec.replace(/^-----BEGIN PRIVATE KEY-----/,"").replace(/-----END PRIVATE KEY-----$/,""));
   return dec.replace(/^-----BEGIN PRIVATE KEY-----/,"").replace(/-----END PRIVATE KEY-----$/,"");
 }
 
@@ -51,7 +306,6 @@ function PullMyPublic(password){
   var hash = HashFromPass(password);
   var filename = "./keys/public/"+hash+".txt";
   var public_file = fs.readFileSync(filename,'utf-8');
-  console.log(public_file.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,""));
   return public_file.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,"");
 }
 
@@ -64,7 +318,6 @@ function PublicFromPrivate(Private){
   }
   var Public = key.exportKey('pkcs8-public-pem');
   var edit_public =  Public.replace(/^-----BEGIN PUBLIC KEY-----/,"").replace(/-----END PUBLIC KEY-----$/,"");
-  console.log(edit_public);
   return edit_public
 }
 
@@ -78,7 +331,6 @@ function EncryptData(data,Public){
     var key = new NodeRSA(Public);
   }
   var encrypted = key.encrypt(data, 'base64');
-  console.log('encrypted: ', encrypted);
   return encrypted;
 }
 
@@ -89,11 +341,9 @@ function DecryptData(data,Private){
     var key = new NodeRSA(Private);
   }
   else{
-    console.log(Private);
     var key = new NodeRSA(Private);
   }
   var decrypted = key.decrypted(data,'utf-8');
-  console.log('decrypted: ', decrypted);
   return decrypted;
 }
 
@@ -116,7 +366,31 @@ function verifyData(data,sign,Public){
   }
   return key.verify(data, sign);
 }
-
+function AddressFromPublic(Public) {
+  if(Public.match(BofPub)&&Public.match(EofPub)) {
+    Public.replace(BofPub,"").replace(EofPub,"");
+  }
+  var hashed = HashFromPass(Public).substr(0,60);
+  hashed = HashFromPass(hashed).substr(0,30);
+  var address = "PH"+hashed;
+  return address;
+}
+function SystemAddress(name){
+  var add;
+  if(Buffer.byteLength(name,'utf-8')>30){
+    name.substr(0,30);
+    add=28;
+  }
+  else if(Buffer.byteLength(name,'utf-8')>=0){
+    add=58-Buffer.byteLength(name,'utf-8');
+  }
+  else{
+    return false;
+  }
+  var hashed = HashFromPass(name).substr(0,add);
+  var address = 'PS' + name + hashed;
+  return address;
+}
 module.exports ={
   HashFromPass:HashFromPass,
   GenerateKeys:GenerateKeys,
@@ -126,5 +400,7 @@ module.exports ={
   EncryptData:EncryptData,
   DecryptData:DecryptData,
   SignData:SignData,
-  verifyData:verifyData
+  verifyData:verifyData,
+  AddressFromPublic:AddressFromPublic
 };
+*/
